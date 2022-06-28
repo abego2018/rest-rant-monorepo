1dc869eb1a6aab49593e59d23cd4bceef67686bc
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-06-27 18:56:49 -0700

    pending work on jwt authentication

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -27,13 +27,14 @@ function LoginForm() {
        
         const data = await response.json()
 
-        // if (response.status === 200){
-        //     setCurrentUser(data.user)
-        //     history.push('/')
-        // }
-        // else {
-        //     setErrorMessage(data.message)
-        // }
+        if (response.status === 200){
+            setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
+            history.push('/')
+        }
+        else {
+            setErrorMessage(data.message)
+        }
     }
 
     return (
